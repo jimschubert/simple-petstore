2fc2385f94dea2d2a0b7c8a96512ac3d9c23f4f4
Jim Schubert <james.schubert@gmail.com>
2017-06-04 21:24:22 -0400

    Configure customized pet endpoints

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
 const jsonServer = require('json-server');
 const path = require('path');
-
+const express = require('express');
+const util     = require('util');
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
-const middlewares = jsonServer.defaults();
+const defaultMiddleware = jsonServer.defaults();
+const apiPrefix = '/api';
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares);
+server.use(defaultMiddleware);
 
 // Add custom routes before JSON Server router
 server.get('/echo', (req, res) => {
@@ -16,16 +18,19 @@ server.get('/echo', (req, res) => {
 
 server.use(jsonServer.bodyParser);
 
-server.use(jsonServer.rewriter({
-    "/api/": "/",
-    "/api/pets": "/pets",
-    "/api/tags": "/tags",
-    "/api/orders": "/orders",
-    "/api/users": "/users",
-    "/api/categories": "/categories"
-}));
+server.get(`${apiPrefix}/pets/findByStatus`, function(req, res, next){
+    req.url = `${apiPrefix}/pets`;
+    let query = req.query;
+    // Allow for csv format (defined by swagger) and multi (express built-in)
+    if(util.isString(query.status)){
+        req.query = {
+            status: query.status.split(',')
+        };
+    }
+    next();
+});
 
-server.use(router);
+server.use(apiPrefix, router);
 
 server.listen(3000, () => {
     require('express-route-log')(server);
